93d8de1e69c7c13d58a99f608bed70a93d32f7c6
Hassan <hassanisavailable@gmail.com>
2026-08-04 09:13:53 +0500

    migration: create departments table schema

---
+++ db/migrate/20260804000001_create_departments.rb
@@ -0,0 +1,13 @@
+class CreateDepartments < ActiveRecord::Migration[7.1]
+  def change
+    create_table :departments do |t|
+      t.string :name, null: false
+      t.string :code, null: false
+      t.text :description
+      t.boolean :active, default: true, null: false
+
+      t.timestamps
+    end
+    add_index :departments, :code, unique: true
+  end
+end
